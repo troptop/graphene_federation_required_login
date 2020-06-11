238d7f07ac41de007e1cc563ba88c4cbb1a7fc3a
Cyril Moreau <cyril.moreauu@gmail.com>
2020-06-11 20:11:20 +0100

    middleware bug

--- graphene_federation_required_login/middleware.py
+++ graphene_federation_required_login/middleware.py
@@ -7,12 +7,7 @@ logger = logging.getLogger(__name__)
 class AuthorizationMiddleware(object):
     def resolve(self, next, root, info, **kwargs):
         _GRAPHENE_FEDERATION_HEADER = getattr(settings,'GRAPHENE_FEDERATION_HEADER','GRAPHENE_FEDERATION_HEADER')
-        if settings.DEBUG:
-            logger.error('graphene_federation_required_login - middleware : {}'.format(info.context.headers.items()))
         setattr(info.context, _GRAPHENE_FEDERATION_HEADER, None)
-        if 'user' in info.context.headers:
-            #logger.error('-------  USER ------- {}'.format(info.context.headers['user']))
+        if _GRAPHENE_FEDERATION_HEADER in info.context.headers:
             setattr(info.context, _GRAPHENE_FEDERATION_HEADER, json.loads(info.context.headers[_GRAPHENE_FEDERATION_HEADER]))
-        if settings.DEBUG:
-            logger.error('graphene_federation_required_login - middleware : {}'.format(getattr(info.context,_GRAPHENE_FEDERATION_HEADER)))        
         return next(root, info, **kwargs)
