de135d0aff0db9129381e41d00935b9710a6c9e1
Cyril Moreau <cyril.moreauu@gmail.com>
2020-06-12 19:41:49 +0100

    middleware bug

--- graphene_federation_required_login/middleware.py
+++ graphene_federation_required_login/middleware.py
@@ -8,14 +8,9 @@ logger = logging.getLogger(__name__)
 
 class AuthorizationMiddleware(object):
     def resolve(self, next, root, info, **kwargs):
-        logger.error('-------  HEADER ------- {}'.format(info.context.headers.items()))
         gfrl_federation_header = getattr(settings,SETTINGS_GFRL_FEDERATION_HEADER_ATTRIBUTE,DEFAULT_GFRL_FEDERATION_HEADER)
         gfrl_django_context = getattr(settings,SETTINGS_GFRL_DJANGO_CONTEXT_ATTRIBUTE,DEFAULT_GFRL_DJANGO_CONTEXT)
-        logger.error('-------  gfrl_federation_header ------- {}'.format(gfrl_federation_header))
         setattr(info.context, gfrl_django_context, None)
-        logger.error('-------  _GETATTRIBUTE ------- {}'.format(getattr(info.context, gfrl_django_context,"NOT WORKING")))
         if gfrl_federation_header in info.context.headers:
             setattr(info.context, gfrl_django_context, json.loads(info.context.headers[gfrl_federation_header]))
-            logger.error('-------  MIDDLEWARE ------- {}'.format(dir(info.context)))
-            logger.error('-------  MIDDLEWARE ------- {}'.format(info.context.__dict__[gfrl_django_context]))
         return next(root, info, **kwargs)
