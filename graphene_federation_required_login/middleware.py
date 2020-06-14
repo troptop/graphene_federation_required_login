b91e55a2f8804396b684663889b300f52d5f9394
Cyril Moreau <cyril.moreauu@gmail.com>
2020-06-14 12:30:08 +0100

    fix bug middleware

--- graphene_federation_required_login/middleware.py
+++ graphene_federation_required_login/middleware.py
@@ -11,6 +11,6 @@ class AuthorizationMiddleware(object):
         gfrl_federation_header = getattr(settings,SETTINGS_GFRL_FEDERATION_HEADER_ATTRIBUTE,DEFAULT_GFRL_FEDERATION_HEADER)
         gfrl_django_context = getattr(settings,SETTINGS_GFRL_DJANGO_CONTEXT_ATTRIBUTE,DEFAULT_GFRL_DJANGO_CONTEXT)
         setattr(info.context, gfrl_django_context, None)
-        if gfrl_federation_header in info.context.headers:
+        if gfrl_federation_header in info.context.headers and info.context.headers[gfrl_federation_header]:
             setattr(info.context, gfrl_django_context, json.loads(info.context.headers[gfrl_federation_header]))
         return next(root, info, **kwargs)
