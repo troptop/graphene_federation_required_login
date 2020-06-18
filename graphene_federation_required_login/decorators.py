339a13ff12624d180071ff98ba6c0d1976d7fcd6
Cyril Moreau <cyril.moreauu@gmail.com>
2020-06-18 20:59:50 +0100

    fix bug middleware

--- graphene_federation_required_login/decorators.py
+++ graphene_federation_required_login/decorators.py
@@ -3,10 +3,18 @@ from . import exceptions
 from functools import wraps
 from django.conf import settings
 from .apps import (DEFAULT_GFRL_DJANGO_CONTEXT , SETTINGS_GFRL_DJANGO_CONTEXT_ATTRIBUTE)
+def context(f):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            info = args[f.__code__.co_varnames.index('info')]
+            return func(info, *args, **kwargs)
+        return wrapper
+    return decorator
 
 def required_federation_login(_func=None, *, header=getattr(settings,SETTINGS_GFRL_DJANGO_CONTEXT_ATTRIBUTE,DEFAULT_GFRL_DJANGO_CONTEXT)):
     def decorator(view_func):
         @wraps(view_func)
+        @context(view_func)
         def _wrapped_view(self, info, **kwargs):
             if hasattr(info.context,header) and getattr(info.context,header):
                 return view_func(self, info, **kwargs)
